65fa8ddc8d35f8c6584311a76924aa4c463827f2
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2025-12-30 13:54:01 +0100

    website: redirect on index

--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -1,30 +1,13 @@
-import { createFileRoute, Link, useRouter } from "@tanstack/react-router"
-import { Button } from "@/components/ui/button.tsx"
+import { createFileRoute, redirect } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
-  component: Index,
+  beforeLoad: ({ context }) => {
+    if (!context.auth.authenticated) {
+      throw redirect({
+        to: "/login",
+      })
+    }
+
+    throw redirect({ to: "/dashboard" })
+  },
 })
-
-function Index() {
-  const { auth } = Route.useRouteContext()
-  const r = useRouter()
-  return (
-    <div className="p-2 flex flex-col">
-      <Link to="/login">login</Link>
-
-      <Link to="/dashboard">dashboard</Link>
-
-      {auth.authenticated ? (
-        <Button
-          variant="destructive"
-          onClick={() => {
-            auth.logout()
-            r.invalidate()
-          }}
-        >
-          Log out
-        </Button>
-      ) : null}
-    </div>
-  )
-}
