f92351a7e7d6f9279f32ef9cba9663a811985cdf
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2026-01-09 16:59:11 +0100

    chore: spotless

--- api/src/test/kotlin/shortie/tests/services/UrlsServiceTest.kt
+++ api/src/test/kotlin/shortie/tests/services/UrlsServiceTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2026 Krzysztof Borowy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 @file:OptIn(ExperimentalTime::class)
 
 package shortie.tests.services
@@ -93,9 +108,7 @@ class UrlsServiceTest {
             service.verifyPassword(shortie.shortCode, "my wrong password")
         }
 
-        assertTrue("passwords should match") {
-            service.verifyPassword(shortie.shortCode, password)
-        }
+        assertTrue("passwords should match") { service.verifyPassword(shortie.shortCode, password) }
     }
 
     @Test
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,19 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ktor) apply false
+    alias(libs.plugins.spotless)
+}
+
+
+spotless {
+    val buildDirectory = layout.buildDirectory.asFileTree
+    val licenseFile = rootProject.file("spotless/license.txt")
+    kotlin {
+        target("api/**/*.kt", "api/**/*.kts")
+        ktfmt().kotlinlangStyle()
+        targetExclude(buildDirectory, licenseFile, "**/*.kts", "frontend")
+        licenseHeaderFile(licenseFile)
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
