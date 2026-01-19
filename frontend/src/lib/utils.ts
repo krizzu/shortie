6678ff5bce055a70e739edb5514d0cc9b92b7d5d
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2026-01-19 22:08:26 +0100

    connect query

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function dateToUtcDateString(d: Date): string {
+  return d.toISOString().slice(0, 10)
 }
--- frontend/src/routes/_authorized/dashboard.analytics/$shortCode.tsx
+++ frontend/src/routes/_authorized/dashboard.analytics/$shortCode.tsx
@@ -1,13 +1,61 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute, useRouter } from "@tanstack/react-router"
+import { linkAnalyticsQueryOptions } from "@/queries/links-analytics-query-options.ts"
+import { useQuery } from "@tanstack/react-query"
+import { dateToUtcDateString } from "@/lib/utils.ts"
+import { Error } from "@/components/Error.tsx"
+import { Loading } from "@/components/Loading.tsx"
 
 export const Route = createFileRoute(
   "/_authorized/dashboard/analytics/$shortCode"
 )({
+  validateSearch: (raw): { startDate?: string; endDate?: string } => {
+    return {
+      startDate: raw["startDate"] ? String(raw["startDate"]) : undefined,
+      endDate: raw["endDate"] ? String(raw["endDate"]) : undefined,
+    }
+  },
+
+  loaderDeps: ({ search }) => {
+    const today = dateToUtcDateString(new Date())
+    const next7Days = dateToUtcDateString(new Date(Date.now() + 604800))
+
+    return {
+      startDate: search?.startDate ?? today,
+      endDate: search?.endDate ?? next7Days,
+    }
+  },
+
+  loader: ({ params, context, deps }) => {
+    context.queryClient.ensureQueryData(
+      linkAnalyticsQueryOptions(params.shortCode, deps.startDate, deps.endDate)
+    )
+  },
+
   component: RouteComponent,
 })
 
 function RouteComponent() {
   const { shortCode } = Route.useParams()
+  const { startDate, endDate } = Route.useLoaderDeps()
+  const router = useRouter()
+  const query = useQuery(
+    linkAnalyticsQueryOptions(shortCode, startDate, endDate)
+  )
+
+  if (query.isLoading) {
+    return <Loading label="loading data..." />
+  }
+
+  if (query.error) {
+    return (
+      <Error
+        error={query.error}
+        reset={() => {
+          router.invalidate()
+        }}
+      />
+    )
+  }
 
   return (
     <div>
