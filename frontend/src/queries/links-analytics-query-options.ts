d4917fe5b4413a769b2542b43f2c9b88ac759bc3
Krzysztof Borowy <6444719+krizzu@users.noreply.github.com>
2026-01-21 17:18:41 +0100

    prepare fetching link list

--- frontend/src/queries/links-analytics-query-options.ts
+++ frontend/src/queries/links-analytics-query-options.ts
@@ -2,6 +2,12 @@ import { queryOptions } from "@tanstack/react-query"
 import { fetcher } from "@/services/fetcher.ts"
 import type { ShortieLinkAnalytic } from "@/types/Link.ts"
 
+export type ResponsePaginatedLinksAnalytics = {
+  hasNext: boolean
+  nextCursor: string | null
+  data: Array<ShortieLinkAnalytic>
+}
+
 export const linkAnalyticsQueryOptions = (
   cursor: string | undefined,
   limit: number | undefined = 20
@@ -14,7 +20,7 @@ export const linkAnalyticsQueryOptions = (
         url += `&cursor=${encodeURIComponent(cursor)}`
       }
 
-      const apiResult = await fetcher<ShortieLinkAnalytic>(url, {
+      const apiResult = await fetcher<ResponsePaginatedLinksAnalytics>(url, {
         method: "GET",
       })
 
--- frontend/src/routes/_authorized/dashboard.analytics/index.tsx
+++ frontend/src/routes/_authorized/dashboard.analytics/index.tsx
@@ -1,16 +1,42 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { linkAnalyticsQueryOptions } from "@/queries/links-analytics-query-options.ts"
+import { useQuery } from "@tanstack/react-query"
+
+const DEFAULT_LIMIT = 20
 
 export const Route = createFileRoute("/_authorized/dashboard/analytics/")({
+  validateSearch: (
+    raw
+  ): { limit?: number; page?: string; previous?: string[] } => {
+    return {
+      limit: isNaN(Number(raw["limit"])) ? DEFAULT_LIMIT : Number(raw["limit"]),
+      page: raw["page"] ? String(raw["page"]) : undefined,
+      previous: raw["previous"]
+        ? String(raw["previous"]).split(",")
+        : undefined,
+    }
+  },
+
+  loaderDeps: ({ search }) => {
+    return {
+      limit: search.limit,
+      page: search.page,
+    }
+  },
+
+  loader: ({ context, deps }) => {
+    context.queryClient.ensureQueryData(
+      linkAnalyticsQueryOptions(deps.page, deps.limit)
+    )
+  },
+
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return (
-    <div>
-       main analytics page:
-      - total links
-      - total clicks
-      - avg clicks
-    </div>
-  )
+  const deps = Route.useLoaderDeps()
+  const links = useQuery(linkAnalyticsQueryOptions(deps.page, deps.limit))
+
+  // todo: build page
+  return <div>{links.data?.data?.map((l) => l.shortCode)}</div>
 }
